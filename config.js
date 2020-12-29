496a81a3946a08fe3eec82e01662475a1e04ed5d
Preet <preetpal.dhillon1@gmail.com>
2020-12-29 13:20:29 +1100

    update pass

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   USER: "rimmwedspreet@gmail.com",
-  PASS: "Lz91$2ISjVJHzD&O",
+  PASS:
+    "2&Kly#3RxmzvDf2SP%ofUq4j4VSG0rLoSe3xNtkqyAon8bJyZxo8l307!&sJViAqLsGtA7mHsrn$cSvTS8kl2mNE5uSGyz141LU",
   RSVP_PASS: "rimshipreetengagement",
 };
